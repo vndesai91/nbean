6529eb07fe59a39b33ea920253eda9d8761418f5
vndesai91 <59364833+vndesai91@users.noreply.github.com>
2020-01-31 21:59:30 +0530

    Update value.java

--- src/main/java/value.java
+++ src/main/java/value.java
@@ -2,10 +2,9 @@ public class value {
   public static void main(String[] args) {
         int base = 3, exponent = 4;
         long result = 1;
-        while (exponent != 0)
+        for (;exponent != 0; --exponent)
         {
             result *= base;
-            --exponent;
         }
         System.out.println("Answer = " + result);
     }
